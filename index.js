92b4965e7daf09846a74626f5cf3fdd9a6a5602e
prakasham-om <prakashchandrasahoo8991@gmail.com>
2024-03-07 17:37:03 +0530

    hello

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://your_mongodb_connection_string', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://rohitsahoo866:prakash1998@cluster0.tynrt9h.mongodb.net/dataneuron?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
